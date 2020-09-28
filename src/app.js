4d14ad14282cb160f5e0708e4020e20c2b90651a
Amaldev <amaldevm19@gmail.com>
2020-09-28 10:27:47 -0700

    Installed express and created express server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+var logger = require('morgan');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var router = require('./routes');
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use('/api/v1', router);
+
+
+//Creating an express server
+const port = 3000;
+app.listen(port, (err)=>{
+    if(err) console.error("Error ");
+    console.log(`Express API is listening at port : ${port}`)
+})
