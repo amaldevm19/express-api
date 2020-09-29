1ea89bdd9bf2212dfb6012387a4e3170121334a1
Amaldev <amaldevm19@gmail.com>
2020-09-28 19:46:16 -0700

    Added environment variables

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 var express = require('express');
+const testEnvironmentVariable  = require("./config")
 var router = express.Router();
 
 router.get('/', (req, res, next)=>{
-    return res.status(200).json({message:"Welcome to Express API"})
+    return res.status(200).json({message: testEnvironmentVariable })
 })
 
 module.exports = router;
